435334795ec1f999354d6f5413c4bc30909fe342
Anna Notovska <annanotovska@gmail.com>
2025-08-04 08:46:30 +0300

    changed routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Dashboard userTier={userTier} onTierChange={handleTierChange} />} />
         <Route path="/welcome" element={<Welcome userTier={userTier} onTierChange={handleTierChange} />} />
